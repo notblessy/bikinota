dd46d7be8b2baa2d0c7b0aa7c4bea2880f1df015
Frederich Blessy <frederichblessy@gmail.com>
2024-05-18 22:42:44 +0700

    feature: recipient information

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,16 +1,9 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  Divider,
-  Group,
-  Text,
-  Title,
-} from "@mantine/core";
+import { Box } from "@mantine/core";
 
 import { useState } from "react";
 
 import { BasicInfo } from "./BasicInfo";
+import { RecipientInfo } from "./RecipientInfo";
 
 export const Create = () => {
   const [step, setStep] = useState(1);
@@ -18,21 +11,7 @@ export const Create = () => {
   return (
     <Box>
       {step === 1 && <BasicInfo step={step} setStep={setStep} />}
-      {step === 2 && (
-        <Box>
-          <Title order={1}>Step 2: Additional Information</Title>
-          <Divider />
-          <Group>
-            <Avatar src={localStorage.getItem("basicInfo").photoURL} />
-            <Box>
-              <Text>{localStorage.getItem("basicInfo").name}</Text>
-              <Text>{localStorage.getItem("basicInfo").phone}</Text>
-              <Text>{localStorage.getItem("basicInfo").address}</Text>
-              <Button onClick={() => setStep(1)}>Back</Button>
-            </Box>
-          </Group>
-        </Box>
-      )}
+      {step === 2 && <RecipientInfo step={step} setStep={setStep} />}
     </Box>
   );
 };
---
+++ src/pages/RecipientInfo.jsx
@@ -0,0 +1,85 @@
+import { Box, Button, Group, TextInput, Textarea, Title } from "@mantine/core";
+
+import { useForm } from "@mantine/form";
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+export const RecipientInfo = ({ step, setStep }) => {
+  const [loading, setLoading] = useState(false);
+
+  const storedRecipientInfo = localStorage.getItem("recipientInfo");
+  const recipientInfo = storedRecipientInfo
+    ? JSON.parse(storedRecipientInfo)
+    : {};
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      name: recipientInfo.name,
+      phone: recipientInfo.phone,
+      photo: recipientInfo.photo,
+    },
+
+    validate: {},
+  });
+
+  const handleSubmit = (values) => {
+    setLoading(true);
+
+    localStorage.setItem("recipientInfo", JSON.stringify(values));
+
+    setStep(step + 1);
+    setLoading(false);
+  };
+
+  return (
+    <Box>
+      <Title order={3}>Recipient Information</Title>
+      <Box>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Title order={6} c="dimmed">
+            Fill recipient information.
+          </Title>
+          <TextInput
+            my={10}
+            withAsterisk
+            label="Name"
+            placeholder="Name"
+            key={form.key("name")}
+            {...form.getInputProps("name")}
+          />
+
+          <TextInput
+            withAsterisk
+            label="Phone"
+            placeholder="Phone"
+            key={form.key("phone")}
+            {...form.getInputProps("phone")}
+          />
+
+          <Textarea
+            mt={10}
+            label="Address"
+            placeholder="Address (optional)"
+            key={form.key("address")}
+            {...form.getInputProps("address")}
+          />
+
+          <Group justify="flex-end" mt="md" grow>
+            <Button
+              loading={loading}
+              onClick={() => setStep(1)}
+              variant="light"
+              color="red"
+            >
+              Back
+            </Button>
+            <Button loading={loading} type="submit" color="red">
+              Next
+            </Button>
+          </Group>
+        </form>
+      </Box>
+    </Box>
+  );
+};
